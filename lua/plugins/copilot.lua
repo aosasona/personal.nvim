42ec294209adc282cef779b67189f05302e6b39d
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-11-16 18:40:08 +0000

    Disabled copilot suggestions

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,21 +1,22 @@
 return {
-	"zbirenbaum/copilot.lua",
-	cmd = "Copilot",
-	event = "InsertEnter",
-	config = function()
-		require("copilot").setup({
-			panel = {
-				auto_refresh = true,
-			},
-			suggestion = {
-				auto_trigger = true,
-				keymap = {
-					accept = "<Tab>",
-					dismiss = "<C-]>",
-					next = "<C-j>",
-					prev = "<C-k>",
-				}
-			}
-		})
-	end,
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				panel = {
+					enabled = false,
+					auto_refresh = true,
+				},
+				suggestion = { enabled = false, }
+			})
+		end,
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		config = function()
+			require("copilot_cmp").setup()
+		end
+	}
 }
