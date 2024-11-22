f2a0239a7e765a431abc33fc9c7baf437a3989b7
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-11-22 17:40:18 +0000

    Removed custom autocmd for formatting

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -31,18 +31,11 @@ function M.setup()
 		end,
 	})
 
-	-- Autoformat on save
-	vim.api.nvim_create_autocmd("BufWritePre", {
-		callback = function()
-			vim.lsp.buf.format({ async = false })
-		end,
-	})
-
 	-- Go imports and formatting on save
 	vim.api.nvim_create_autocmd("BufWritePre", {
 		pattern = "*.go",
 		callback = function()
-			require('go.format').goimports()
+			require("go.format").goimports()
 		end,
 		group = vim.api.nvim_create_augroup("GoFormat", {}),
 	})
