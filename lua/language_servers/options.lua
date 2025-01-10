30cd393cd1f339812f82fb724b347add716474e3
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-01-10 16:24:45 +0000

    Added sourcekit support

--- lua/language_servers/options.lua
+++ lua/language_servers/options.lua
@@ -1,73 +1,94 @@
-local options = {
-	["lua_ls"] = {
-		on_init = function(client)
-			if client.workspace_folders then
-				local path = client.workspace_folders[1].name
-				if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
-					return
+local config = {
+	options = {
+		["lua_ls"] = {
+			on_init = function(client)
+				if client.workspace_folders then
+					local path = client.workspace_folders[1].name
+					if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+						return
+					end
 				end
-			end
 
-			client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-				runtime = {
-					version = "LuaJIT",
-				},
-				-- Make the server aware of Neovim runtime files
-				workspace = {
-					checkThirdParty = false,
-					library = {
-						vim.env.VIMRUNTIME,
+				client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+					runtime = {
+						version = "LuaJIT",
 					},
-				},
-			})
-		end,
+					-- Make the server aware of Neovim runtime files
+					workspace = {
+						checkThirdParty = false,
+						library = {
+							vim.env.VIMRUNTIME,
+						},
+					},
+				})
+			end,
 
-		settings = {
-			Lua = {
-				runtime = {
-					version = "LuaJIT",
-					path = vim.split(package.path, ";"),
-				},
-				diagnostics = {
-					globals = { "vim" },
+			settings = {
+				Lua = {
+					runtime = {
+						version = "LuaJIT",
+						path = vim.split(package.path, ";"),
+					},
+					diagnostics = {
+						globals = { "vim" },
+					},
 				},
 			},
 		},
-	},
-	["tinymist"] = {
-		settings = {
-			offset_encoding = "utf-8",
-			exportPdf = "onSave",
+		["tinymist"] = {
+			settings = {
+				offset_encoding = "utf-8",
+				exportPdf = "onSave",
+			},
+		},
+		["v_analyzer"] = {},
+		["sqls"] = {
+			cmd = { "sqls", "--config", "/Users/ayodeji/.config/sqls/config.yml" },
+		},
+		["gleam"] = {},
+		["sourcekit"] = {
+			capabilities = {
+				workspace = {
+					didChangeWatchedFiles = {
+						dynamicRegistration = true,
+					},
+				},
+			},
+			root_dir = function(filename, _)
+				local util = require("lspconfig.util")
+				return util.root_pattern("buildServer.json")(filename)
+					or util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
+					or util.root_pattern("Package.swift")(filename)
+					or util.find_git_ancestor(filename)
+			end,
 		},
 	},
 
-	["sqls"] = {
-		cmd = { "sqls", "--config", "/Users/ayodeji/.config/sqls/config.yml" },
+	-- List of servers that are installed locally (i.e. not installed with mason)
+	local_servers = {
+		"v_analyzer",
+		"sqls",
+		"gleam",
+		"sourcekit",
 	},
-
-	["v_analyzer"] = {},
 }
 
 local M = {}
 
 function M.get_opts(server_name)
-	return options[server_name]
+	return config.options[server_name]
 end
 
 function M.merge_opts(server_name, opts)
-	if options[server_name] then
-		return vim.tbl_deep_extend("force", options[server_name], opts)
+	if config.options[server_name] then
+		return vim.tbl_deep_extend("force", config.options[server_name], opts)
 	end
 
 	return opts
 end
 
 function M.local_servers()
-	return {
-		"v_analyzer",
-		"sqls",
-		"gleam",
-	}
+	return config.local_servers
 end
 
 return M
