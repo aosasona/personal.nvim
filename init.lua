7e985191937d324f2249778b2b07b863a3ab7649
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-04-01 18:02:19 +0100

    Switch to oxocarbon theme

--- init.lua
+++ init.lua
@@ -33,6 +33,6 @@ require("core.opts").load()
 -- Use nvim-notify as default notification handler
 vim.notify = require("notify")
 -- vim.cmd.colorscheme("gruvbox-material")
-vim.cmd.colorscheme("catppuccin-mocha")
+vim.cmd.colorscheme("oxocarbon")
 
 require("core").setup()
