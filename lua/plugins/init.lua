46617dfca66c8a36f62f4e4cd154bde48d75930d
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-04-06 14:43:37 +0100

    Move themes to plugins/colors directory

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,23 +4,35 @@ local function is_array(table)
 	return type(table) == "table" and table[1] ~= nil
 end
 
--- Read all files in the plugins directory except for the init.lua file
-local files = vim.fn.globpath(vim.fn.stdpath("config") .. "/lua/plugins", "*.lua", false, true)
+-- Load plugin definitions from a given folder (e.g. plugins or colors)
+local function load_plugins_from(dir_name)
+	local base_path = vim.fn.stdpath("config") .. "/lua/" .. dir_name
 
-for _, file in ipairs(files) do
-	if file:match("init.lua") == nil then
-		local plugin_name = file:match("plugins/(.*)"):gsub(".lua", "")
-		local plugin = require("plugins." .. plugin_name)
+	-- Check outside of the lua folder
+	if vim.fn.isdirectory(base_path) == 0 then
+		base_path = vim.fn.stdpath("config") .. "/" .. dir_name
+	end
 
-		-- If it is an array of plugins, add them all
-		if is_array(plugin) then
-			for _, plugin_definition in ipairs(plugin) do
-				table.insert(plugins, plugin_definition)
+	local files = vim.fn.globpath(base_path, "*.lua", false, true)
+
+	for _, file in ipairs(files) do
+		if not file:match("init.lua") then
+			local plugin_name = file:match(dir_name .. "/(.*)"):gsub(".lua", "")
+			local plugin = require(dir_name .. "." .. plugin_name)
+
+			if is_array(plugin) then
+				for _, plugin_definition in ipairs(plugin) do
+					table.insert(plugins, plugin_definition)
+				end
+			else
+				table.insert(plugins, plugin)
 			end
-		else
-			table.insert(plugins, plugin)
 		end
 	end
 end
+
+-- Load plugins from both plugins and colors folders
+load_plugins_from("plugins")
+load_plugins_from("plugins/colors")
 
 return plugins
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -61,33 +61,6 @@ return {
 			require("kulala").setup()
 		end,
 	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		lazy = false,
-		branch = "master",
-		build = ":TSUpdate",
-		config = function()
-			local configs = require("nvim-treesitter.configs")
-			configs.setup({
-				ensure_installed = {
-					"phpdoc",
-					"php",
-					"css",
-					"html",
-					"javascript",
-					"lua",
-					"regex",
-					"go",
-					"rust",
-					"typescript",
-					"typst",
-				},
-				sync_install = false,
-				highlight = { enable = true },
-				indent = { enable = true },
-			})
-		end,
-	},
 	{ "tpope/vim-surround" },
 	{
 		"f-person/git-blame.nvim",
