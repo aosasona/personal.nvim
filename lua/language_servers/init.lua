5ca42bd6a7bcac7f3167e4a8dbbf1f9d2ab4ee94
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-05-27 21:51:18 +0100

    Update kulala and mason-lspconfig

--- lua/language_servers/init.lua
+++ lua/language_servers/init.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 function M.setup()
-	local exclude_servers = { "rust_analyzer" }
 	local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 	-- For nvim-ufo
@@ -9,17 +8,6 @@ function M.setup()
 		dynamicRegistration = false,
 		lineFoldingOnly = true,
 	}
-
-	require("mason-lspconfig").setup_handlers({
-		function(server_name)
-			if vim.tbl_contains(exclude_servers, server_name) then
-				return
-			end
-
-			local opts = require("language_servers.options").merge_opts(server_name, { capabilities = capabilities })
-			require("lspconfig")[server_name].setup(opts)
-		end,
-	})
 
 	-- Setup local servers (i.e. not installed with mason)
 	local local_servers = require("language_servers.options").local_servers()
