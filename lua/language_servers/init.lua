a48109a836488c5026e2e26da47afe2d30325711
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2026-06-19 02:45:20 +0100

    Migrate from lspconfig.setup to new vim.lsp API

--- lua/language_servers/init.lua
+++ lua/language_servers/init.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 function M.setup()
+	vim.lsp.inlay_hint.enable(true) -- Enable inlay hints globally
 	local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 	-- For nvim-ufo
@@ -13,7 +14,9 @@ function M.setup()
 	local local_servers = require("language_servers.options").local_servers()
 	for _, server_name in ipairs(local_servers) do
 		local opts = require("language_servers.options").merge_opts(server_name, { capabilities = capabilities })
-		require("lspconfig")[server_name].setup(opts)
+		-- require("lspconfig")[server_name].setup(opts)
+		vim.lsp.config(server_name, opts)
+		vim.lsp.enable(server_name)
 	end
 end
 
